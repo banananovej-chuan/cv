9ac8e21d638d404a9cb69f6d24c07a9dfcf9eea2
bananovej-chuan <59200292+banananovej-chuan@users.noreply.github.com>
2020-11-21 14:24:42 +0100

    Basic structure

--- cv.js
+++ cv.js
@@ -1,5 +1,62 @@
-user  = {
+const user  = {
     firstName: "Viet Anh",
     lastName: "Chu",
+    nickName: "Chuan",
     sex: "Male"
-}
+};
+const contact = {
+    website: "https://www.chuan.cz",
+    email: "ahoj@chuan.cz"
+};
+const education = {
+    bachelorDegree: {
+        school: "Univerzita Jana Evangelisty Purkyně v Ústí nad Labem",
+        fieldOfStudy: "Finanční management",
+        startYear: 2014,
+        endYear: 2017
+    },
+    masterDegree: {
+        school: "Univerzita Jana Evangelisty Purkyně v Ústí nad Labem",
+        fieldOfStudy: "Obchod a marketing",
+        startYear: 2017,
+        endYear: 2019
+    }
+};
+let experience = {
+    universitySenate: {
+        title: "senator",
+        company: "Univerzita Jana Evngelisty Purkyně v Ústí nad Labem",
+        startDate: "10/2015",
+        endDate: "12/2016"
+    },
+    facultySenate: {
+        title: "senator",
+        company: "Fakulta Sociálně Ekonomická - Univerzita Jana Evngelisty Purkyně v Ústí nad Labem",
+        startDate: "10/2015",
+        endDate: "11/2016"
+    },
+    researchAssistant: {
+        company: "Fakulta Sociálně Ekonomická - Univerzita Jana Evngelisty Purkyně v Ústí nad Labem",
+        startDate: "03/2015",
+        endDate: "05/2017"
+    },
+    internship: {
+        company: "GreenSocs s.r.o",
+        startDate: "08/2018",
+        endDate: "05/2019",
+        jobDescription: "Tvorba reklamních kampaní na sociálních sítích pro vybrané klienty."
+    },
+    socialAdsSpecialist: {
+        company: "eVisions Advertising",
+        startDate: "06/2019",
+        endDate: "10/2020",
+        jobDescription: "Pracovní náplní byla tvorba reklamních kampaní na sociálních sítích (FB, IG, LI), participace v tendrech a tvorba cenových nabídek. Odpovědnost za reportování klientům - tvorba automatizovaného reportu v GDS & Supermetrics."
+    },
+    futureWebAnalyst: {
+        company: "Signals",
+        startDate: "07/2020",
+        endDate: undefined,
+        jobDescription: "Kontrola implementací, tvorba dokumentací."
+    }
+};
+let skills = ["coding", "tech", "wine", "beer", "walking"]
